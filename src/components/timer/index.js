6a70769d550624edd88c3bd465d920882aa4c980
oriastanjung <oriastan999@gmail.com>
2024-09-22 10:49:46 +0700

    feat: timer

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./index.css"
+import  { useEffect, useRef, useState } from "react";
 
-export default function Timer({initial}) {
-	return (
-		<div className="mt-100 layout-column align-items-center justify-content-center">
-			<div className="timer-value" data-testid="timer-value"></div>
-			<button className="large" data-testid="stop-button">Stop Timer</button>
-		</div>
-	);
+export default function Timer({ initial }) {
+  const [count, setCount] = useState(Number(initial));
+  const intervalRef = useRef(null);
 
+  useEffect(() => {
+    if (count > 0) {
+      intervalRef.current = setInterval(() => {
+        setCount((count) => count - 1);
+      }, 1000);
+    }
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  const handleStop = () => {
+    clearInterval(intervalRef.current);
+  }
+  return (
+    <div className="mt-100 layout-column align-items-center justify-content-center">
+      <div className="timer-value" data-testid="timer-value">
+        {count}
+      </div>
+      <button onClick={handleStop} className="large" data-testid="stop-button">
+        Stop Timer
+      </button>
+    </div>
+  );
 }
